a3c314dcf65b09691506b0b8fefc40707892817b
HSzyndlar <hszyndlar@zsp5kozuchow.pl>
2020-10-07 09:33:41 +0200

    Dodanie zdjec do cennika

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const mb = [
         price: 409,
         producent: "Gigabyte",
         socet: "AM4",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:2,
@@ -12,6 +13,7 @@ const mb = [
         price: 769,
         producent: "Gigabyte",
         socet: "AM4",
+        img: "GIGABYTE-X570-Gaming-X.jpg"
     },
     {
         id:3,
@@ -19,6 +21,7 @@ const mb = [
         price: 239,
         producent: "Gigabyte",
         socet: "AM4",
+        img: "GA_A320M_S2H_1.jpg"
     },
 ]
 
@@ -28,24 +31,28 @@ const gfx = [
         name:"MSI GeForce GTX 1650 D6 Gaming X 4GB GDDR6",
         price: 745,
         producent: "MSI",
+        img: "GeForce-GTX-1650.jpg"
     },
     {
         id:2,
         name:"Gigabyte GeForce GTX 1660 Gaming OC 6GB GDDR5",
         price: 999,
         producent: "Gigabyte",
+        img: "Gigabyte-GeForce-GTX-1660.jpg"
     },
     {
         id:3,
         name:"MSI GeForce GTX 1660 GAMING X 6GB GDDR5",
         price: 1139,
         producent: "Gigabyte",
+        img: "msi-gtx-1660-super-gaming-x.jpg"
     },
     {
         id:4,
         name:"Gigabyte GeForce RTX 2060 OC 6GB GDDR6",
         price: 1399,
         producent: "Gigabyte",
+        img: "Gigabyte-GeForce-RTX-2060.jpg"
     },
 ]
 const pro = [
@@ -55,6 +62,7 @@ const pro = [
         price: 699,
         producent: "Intel",
         socet: "1200",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:2,
@@ -62,6 +70,7 @@ const pro = [
         price: 1773,
         producent: "Intel",
         socet: "1200",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:3,
@@ -69,6 +78,7 @@ const pro = [
         price: 929,
         producent: "Amd",
         socet: "AM4",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
 ]
 
@@ -78,18 +88,21 @@ const ram = [
         name:"Gammix D10, DDR4, 16 GB, 3200MHz, CL16 ",
         price: 289,
         producent: "ADATA",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:2,
         name:"IRDM, DDR4, 16 GB, 3600MHz, CL17",
         price: 327,
         producent: "GoodRam ",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:3,
         name:"Vengeance RGB PRO, DDR4, 16 GB, 3200MHz, CL16",
         price: 359,
         producent: "Corsair ",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
 ]
 
@@ -99,18 +112,21 @@ const zasilacz = [
         name:"SilentiumPC Vero L3 500W",
         price: 219,
         producent: "SilentiumPC",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:2,
         name:"be quiet! SYSTEM POWER 9 500W",
         price: 237,
         producent: "be quiet!",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:3,
         name:"SilentiumPC Vero M3 600W ",
         price: 259,
         producent: "SilentiumPC",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
 ]
 const obudowy = [
@@ -119,18 +135,21 @@ const obudowy = [
         name:"SilentiumPC Signum SG7V TG ",
         price: 329,
         producent: "SilentiumPC",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:2,
         name:"SilentiumPC Signum SG7V Evo TG ARGB",
         price: 389,
         producent: "SilentiumPC",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
     {
         id:3,
         name:"Genesis Irid 400 ARGB",
         price: 359,
         producent: "Genesis",
+        img: "GIGABYTE-B450-Aorus-Elite-ATX.jpg"
     },
 ]
 
@@ -184,34 +203,34 @@ function SelectedItemValue(SelectId) {
     const summaryObudowy = document.getElementById("summaryObudowy")
 
     if (SelectId == "cpu") {
-        summaryCpu.innerHTML = " <td>"+ pro[strAtt - 1].name + "</td><td>" + pro[strAtt - 1].price+ "</td>"
+        summaryCpu.innerHTML = "<td><img src='img/"+mb[strAtt - 1].img+"'></td> "+" <td>"+ pro[strAtt - 1].name + "</td><td>" + pro[strAtt - 1].price+ "</td>"
         cart[2].price = pro[strAtt - 1].price
         cart[2].name = pro[strAtt - 1].name
     }
     else if (SelectId == "mb") {
-        summaryMb.innerHTML = "<td>" + mb[strAtt - 1].name + "</td><td>" + mb[strAtt - 1].price+ "</td>"
+        summaryMb.innerHTML ="<td><img src='img/"+mb[strAtt - 1].img+"'></td> "+"<td>" + mb[strAtt - 1].name + "</td><td>" + mb[strAtt - 1].price+ "</td>"
         cart[0].price = mb[strAtt - 1].price
         cart[0].name = mb[strAtt - 1].name
        // document.getElementById("summaryMainboard").innerHTML="mainboard:"+cart[0].name+ " "+cart[0].price;
     }
     else if (SelectId == "gpu") {
-        summaryGfx.innerHTML = "<td>" + gfx[strAtt - 1].name + "</td><td>" + gfx[strAtt - 1].price+ "</td>"
+        summaryGfx.innerHTML = "<td><img src='img/"+gfx[strAtt - 1].img+"'></td> "+" <td>" + gfx[strAtt - 1].name + "</td><td>" + gfx[strAtt - 1].price+ "</td>"
         cart[1].price = gfx[strAtt - 1].price
         cart[1].name = gfx[strAtt - 1].name
     }
 
     else if (SelectId == "zasilacz") {
-        summaryZasilacz.innerHTML = "<td>" + zasilacz[strAtt - 1].name + "</td><td>" + zasilacz[strAtt - 1].price+ "</td>"
+        summaryZasilacz.innerHTML = "<td><img src='img/"+zasilacz[strAtt - 1].img+"'></td> "+" <td>" + zasilacz[strAtt - 1].name + "</td><td>" + zasilacz[strAtt - 1].price+ "</td>"
         cart[1].price = zasilacz[strAtt - 1].price
         cart[1].name = zasilacz[strAtt - 1].name
     }
     else if (SelectId == "obudowy") {
-        summaryObudowy.innerHTML = "<td>"+obudowy[strAtt - 1].name + "</td><td>" + obudowy[strAtt - 1].price+ "</td>"
+        summaryObudowy.innerHTML = "<td><img src='img/"+obudowy[strAtt - 1].img+"'></td> "+" <td>"+obudowy[strAtt - 1].name + "</td><td>" + obudowy[strAtt - 1].price+ "</td>"
         cart[1].price = obudowy[strAtt - 1].price
         cart[1].name = obudowy[strAtt - 1].name
     }
     else if (SelectId == "ram") {
-        summaryObudowy.innerHTML = "<td>"+ram[strAtt - 1].name + "</td><td>" + ram[strAtt - 1].price+ "</td>"
+        summaryObudowy.innerHTML = "<td><img src='img/"+ram[strAtt - 1].img+"'></td> "+" <td>"+ram[strAtt - 1].name + "</td><td>" + ram[strAtt - 1].price+ "</td>"
         cart[1].price = obudowy[strAtt - 1].price
         cart[1].name = obudowy[strAtt - 1].name
     }
